8bed52b2ef139a246988ed117e288d2ae83f1d1b
Parupally Vamshidhar Reddy <42996478+vamshidhar199@users.noreply.github.com>
2022-09-20 11:55:20 -0700

    client

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
 
 function App() {
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {data}
         </p>
         <a
           className="App-link"
