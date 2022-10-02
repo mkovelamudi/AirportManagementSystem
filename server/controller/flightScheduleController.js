c01fb90f75bdfb72c25551bfb481102aa136618c
Gajam-Anurag <anuraggajam21@gmail.com>
2022-10-02 15:55:10 -0700

    flightScheduleController

---
+++ server/controller/flightScheduleController.js
@@ -0,0 +1,18 @@
+// const express = require('express');
+// const mongoose = require('mongoose');
+
+const userModel = require('../models/ScheduledFlight')
+
+exports.getFlightSchedule = async (req,res) => {
+    try{
+        console.log('OK')
+        const data = await userModel.find();
+        if (data){
+        return res.json(data)}
+        return res.json({})
+    }
+    catch(error){
+        console.log(error)
+        return res.status(500).send("Server error")
+    }
+}
