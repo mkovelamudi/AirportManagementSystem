5ae13f688b4ef6d741792286a78a792f73866ffd
Parupally Vamshidhar Reddy <42996478+vamshidhar199@users.noreply.github.com>
2022-10-16 10:50:38 -0700

    added baggage availability drop won and integrated with the api

--- client/src/Table/CustomTable.js
+++ client/src/Table/CustomTable.js
@@ -1,17 +1,31 @@
-import React, { Component,useEffect,useState } from "react";
+import React, { Component, useEffect, useState } from "react";
 import "./CustomTable.css";
+import axios from "axios";
 
 function CustomeTable(props) {
   const [options, setOptions] = useState([]);
-    
-    useEffect(() => {
-        setOptions(["3","4","5"]);
-      }, []);
-      
+
+  useEffect(() => {
+    //setOptions(["3", "4", "5"]);
+  }, []);
+
   const baggageSelect = (x) => {
-    setOptions(["3","4"]);
+    var time=x.arrives;
+    var terminal=x.terminal;
+    var Type="belt";
+    axios
+      .post("/all/availableGates", {
+        date: time,
+        terminal: terminal,
+        type: Type,
+      })
+      .then((res) => {
+        console.log(res);
+        setOptions(res.data);
+      });
     
-    console.log(x);
+
+    // console.log(x);
   };
   return (
     <>
@@ -26,47 +40,46 @@ function CustomeTable(props) {
             } else return <th>{x}</th>;
           })}
         </tr>
-        {props.data && props.data.map((x) => {
-
-          const obj = x;
-            console.log(!props.FromAirport)
-          if(x['type']=="arrival" && props.FromAirport) {
-
-          }
-          else if(x['type']=="departure" && !props.FromAirport) {
-            
-        }
-            else
-          return (
-            <tr>
-              {Object.keys(props.tableNames).map((key) => {
-                if (
-                  props.FromAirport &&
-                  (key == "Arrives" || key == "From" || key == "Baggage Collection")
-                ) {
-                } else if (
-                  !props.FromAirport &&
-                  (key == "Departs" || key == "To")
-                ) {
-                } else if (key == "Baggage Collection") {
-                  return (
-                    <td>
-                      <select
-                        onClick={() => {
-                          baggageSelect(obj);
-                        }}
-                      >
-                        <option selected>{x[props.tableNames[key]]}</option>
-                        {options.map(opt=>{return <option>{opt}</option>})}
-                        
-                      </select>
-                    </td>
-                  );
-                } else return <td>{x[props.tableNames[key]]}</td>;
-              })}
-            </tr>
-          );
-        })}
+        {props.data &&
+          props.data.map((x) => {
+            const obj = x;
+            console.log(!props.FromAirport);
+            if (x["type"] == "arrival" && props.FromAirport) {
+            } else if (x["type"] == "departure" && !props.FromAirport) {
+            } else
+              return (
+                <tr>
+                  {Object.keys(props.tableNames).map((key) => {
+                    if (
+                      props.FromAirport &&
+                      (key == "Arrives" ||
+                        key == "From" ||
+                        key == "Baggage Collection")
+                    ) {
+                    } else if (
+                      !props.FromAirport &&
+                      (key == "Departs" || key == "To")
+                    ) {
+                    } else if (key == "Baggage Collection") {
+                      return (
+                        <td>
+                          <select
+                            onClick={() => {
+                              baggageSelect(obj);
+                            }}
+                          >
+                            <option selected>{x[props.tableNames[key]]}</option>
+                            {options.map((opt) => {
+                              return <option>{opt}</option>;
+                            })}
+                          </select>
+                        </td>
+                      );
+                    } else return <td>{x[props.tableNames[key]]}</td>;
+                  })}
+                </tr>
+              );
+          })}
       </table>
     </>
   );
